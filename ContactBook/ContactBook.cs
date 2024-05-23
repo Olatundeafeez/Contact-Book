5add136cb5c649234b670e1cc28c27375aad0e0f
Olatundeafeez <olatundefolorunshoafeez@gmail.com>
2024-05-23 12:31:02 +0100

    contact book

---
+++ ContactBook/ContactBook.cs
@@ -0,0 +1,42 @@
+﻿using ContactBook;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace ContactBookApp.utility
+{
+    public class ContactBooks
+    {
+
+        Dictionary<string, Contact> newContact = new Dictionary<string, Contact>();
+
+        public void AddContact(Contact contact)
+        {
+            newContact.Add(contact.Name, contact);
+        }
+        public void RemoveContact(string name)
+        {
+            newContact.Remove(name);
+        }
+        public void GetContact(string name)
+        {
+            if (newContact.ContainsKey(name))
+            {
+                newContact.ContainsKey(name);
+            }
+            else
+            {
+                Console.WriteLine("Contact does not exist");
+            }
+        }
+        public void GetAllContact()
+        {
+            foreach (var contact in newContact.Values)
+            {
+                Console.WriteLine($"Name :{contact.Name} and Phone Number {contact.PhoneNumber}");
+            }
+        }
+
+    }
+}
---
+++ ContactBook/Program.cs
@@ -0,0 +1,41 @@
+﻿using ContactBookApp.utility;
+
+var contactBook = new ContactBooks();
+bool exit = false;
+while (!exit)
+{
+    Console.WriteLine("1 : Add a new contact");
+    Console.WriteLine("2 : Remove an existing contact");
+    Console.WriteLine("3 : find a conatct by name");
+    Console.WriteLine("4: list all contact");
+    Console.WriteLine("5 : Exit the program");
+
+
+    Console.WriteLine("enter ypour choice");
+    int choice = int.Parse(Console.ReadLine());
+
+    switch (choice)
+    {
+        case 1:
+            contactBook.AddContact();
+            break;
+        case 2:
+            contactBook.RemoveContact();
+            break;
+        case 3:
+            contactBook.GetContact();
+            break;
+        case 4:
+            exit = true;
+            break;
+        default:
+            Console.WriteLine("you entered an invalid choice");
+            break;
+
+
+    }
+
+}
+
+
+
